09c2e8ee1ca703c1483cf0ba6e60f11dcb08ddff
vle <obentoka@gmail.com>
2020-02-20 13:23:29 -0500

    wrote some more code

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,42 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String fName, String lName, Double[] testScores){
+        this.firstName = fName;
+        this.lastName = lName;
+        for(Double i : testScores){
+            this.examScores.add(i);
+        }
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ClassroomTest {
+
+    @Test
+    public void constructorTest(){
+        String expectedFName = "Von";
+        String expectedLName = "Le";
+        Double[] expectedExamScore = {80.0, 70.0, 95.0};
+        Integer expectedExamAmount = 3;
+
+        Student von = new Student(expectedFName, expectedLName, expectedExamScore);
+
+        String actualFName = von.getFirstName();
+        String actualLName = von.getLastName();
+        Integer actualExamAmount = von.getNumberOfExamsTaken();
+
+        assertEquals(expectedFName, actualFName);
+        assertEquals(expectedLName, actualFName);
+        assertEquals(expectedExamAmount, actualExamAmount);
+        assertEquals(expectedExamScore[0], von.getExamScores().get(0));
+    }
 }
