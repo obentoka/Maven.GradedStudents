3bd5a70b3bf43146276b0fd68d04270f33f6d58c
vle <obentoka@gmail.com>
2020-02-21 12:41:02 -0500

    getStudentByScore done

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,23 +1,27 @@
 package io.zipcoder;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 public class Classroom {
-    private Map<Integer,Student> students;
-    private Integer id = 0;
+
+    private static final Logger LOGGER =
+            Logger.getLogger((Classroom.class.getName()));
+
+    private List<Student> students;
 
     public Classroom(){
-        this.students = new HashMap<>(30);
+        this.students = new ArrayList<>(30);
     }
 
     public Classroom(Integer size){
-        this.students = new HashMap<>(size);
+        this.students = new ArrayList<>(size);
     }
 
     public Classroom(Student[] input){
-        students = new HashMap<>(input.length);
-        for (;this.id < input.length; this.id++) {
-            students.put(this.id, input[this.id]);
+        students = new ArrayList<>(input.length);
+        for (int i = 0; i < input.length; i++) {
+            students.add(input[i]);
         }
     }
 
@@ -38,11 +42,63 @@ public class Classroom {
     }
 
     public void addStudent(Student student){
-        this.students.put(id, student);
-        id++;
+        this.students.add(student);
     }
 
-    public void removeStudent(String firstName, String secondName){
-        //change the collection you are using.
+    public void removeStudent(String firstName, String lastName){
+        if(this.students.size() != 0)
+        for(int i = 0; i < this.students.size(); i++) {
+            if (this.students.get(i).getFirstName().equals(firstName)
+                    && this.students.get(i).getLastName().equals(lastName)) {
+                students.remove(i);
+            }
+        }
+    }
+
+    public Student[] getSudentByScore(){
+        Student[] returnStudents = new Student[this.students.size()];
+        for (int i = 0; i < this.students.size(); i++) {
+            returnStudents[i] = this.students.get(i);
+        }
+
+        for (int i = 1; i < returnStudents.length; i++) {
+            if(returnStudents[i].getAverageExamScore() > returnStudents[i-1].getAverageExamScore()){
+                Student temp = returnStudents[i-1];
+                returnStudents[i-1] = returnStudents[i];
+                returnStudents[i] = temp;
+            }
+        }
+        for (int i = 1; i < returnStudents.length; i++) {
+            if(returnStudents[i].getAverageExamScore().equals(returnStudents[i-1].getAverageExamScore())) {
+                String studentBeforeLastName = returnStudents[i-1].getLastName().toLowerCase();
+                String studentBeforeFirstName = returnStudents[i-1].getFirstName().toLowerCase();
+                String studentAfterLastName = returnStudents[i].getLastName().toLowerCase();
+                String studentAfterFirstName = returnStudents[i].getFirstName().toLowerCase();
+                if (!studentBeforeLastName.equals(studentAfterLastName)) {
+                    int counter = 0;
+                    while (true) {
+                        if (studentBeforeLastName.charAt(counter) > studentAfterLastName.charAt(counter)) {
+                            Student temp = returnStudents[i-1];
+                            returnStudents[i-1] = returnStudents[i];
+                            returnStudents[i] = temp;
+                            break;
+                        }
+                        counter++;
+                    }
+                } else {
+                    int counter = 0;
+                    while (true) {
+                        if (studentBeforeFirstName.charAt(counter) > studentAfterFirstName.charAt(counter)) {
+                            Student temp = returnStudents[i-1];
+                            returnStudents[i-1] = returnStudents[i];
+                            returnStudents[i] = temp;
+                            break;
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+        return returnStudents;
     }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -50,4 +50,45 @@ public class ClassroomTest {
             System.out.println(postEnrollment[i].toString());
         }
     }
+
+    @Test
+    public void removeTest(){
+        int maxNumberOfStudents = 1;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+
+        classroom.addStudent(student);
+        classroom.removeStudent(student.getFirstName(), student.getLastName());
+
+        Integer expectedSize = 0;
+        Integer actualSize = classroom.getStudents().length;
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    public void getStudentByScoreTest(){
+        int maxNumberOfStudents = 2;
+        Classroom classroom = new Classroom(maxNumberOfStudents);
+        Double[] examScores0 = { 100.0, 150.0, 250.0, 0.0 };
+        Student student0 = new Student("Aeon", "Hunter", examScores0);
+        Double[] examScores = { 100.0, 150.0, 250.0, 0.0 };
+        Student student = new Student("Leon", "Hunter", examScores);
+        Double[] examScores2 = { 100.0, 150.0, 250.0, 0.0 };
+        Student student2 = new Student("Leon", "Bunter", examScores2);
+        Double[] examScores3 = { 100.0, 100.0, 100.0, 0.0 };
+        Student student3 = new Student("Test", "Are", examScores3);
+
+        classroom.addStudent(student);
+        classroom.addStudent(student2);
+        classroom.addStudent(student3);
+        classroom.addStudent(student0);
+        Student[] actualStudents = classroom.getSudentByScore();
+        for (int i = 0; i < actualStudents.length; i++) {
+            System.out.println("Student Name: " + actualStudents[i].getFirstName()
+                    + " " + actualStudents[i].getLastName());
+            System.out.println(actualStudents[i].toString());
+        }
+    }
 }
