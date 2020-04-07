c2d267ea768a0dce007e3837466e86b63e122e68
vle <obentoka@gmail.com>
2020-04-07 04:10:55 -0400

    refactored/update gitignore

--- src/main/java/io/zipcoder/CompareStudents.java
+++ src/main/java/io/zipcoder/CompareStudents.java
@@ -2,7 +2,7 @@ package io.zipcoder;
 
 import java.util.Comparator;
 
-public class CompareStudents implements Comparator<Student> {
+public class CompareStudents implements Comparator<Student>{
 
     @Override
     public int compare(Student o1, Student o2){
